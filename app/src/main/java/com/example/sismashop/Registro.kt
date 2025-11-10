950f8a3ea4bec5bc7d19052e49bf9561b24d6efa
adriandiazrulloredondoo <adriandiaz1516@gmail.com>
2025-11-10 20:52:46 +0100

    ah

---
+++ app/src/main/java/com/example/sismashop/Registro.kt
@@ -0,0 +1,243 @@
+package com.example.sismashop
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Registro() {
+    var nombre by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+    val rojo = Color(0xFFC8102E)
+    val negro = Color.Black
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp)
+                .background(Color.White)
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Icon(Icons.Filled.ChevronLeft,
+                contentDescription = "Back",
+                modifier = Modifier
+                    .padding(start = 5.dp)
+                    .align(Alignment.Start)
+                    .size(40.dp)
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                "Registro",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = negro,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .align(Alignment.Start)
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Row(
+                modifier = Modifier.align(Alignment.Start).padding(start = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Crea una cuenta en la tienda de ", color = negro)
+                Text(
+                    text = "Sisma",
+                    fontWeight = FontWeight.Bold,
+                    color = negro
+                )
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+
+                Text("Nombre",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = negro,
+                    modifier = Modifier
+                        .align(Alignment.Start))
+                //El OutlinedTextField para el username
+                OutlinedTextField(
+                    value = nombre,
+                    onValueChange = { nombre = it },
+                    label = { Text("Introduce tu nombre", color = negro.copy(alpha = 0.7f)) },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = rojo,
+                        focusedBorderColor = rojo,
+                        cursorColor = rojo,
+                        unfocusedLabelColor = negro,
+                        focusedLabelColor = negro
+                    )
+                )
+
+                Text("Email",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = negro,
+                    modifier = Modifier
+                        .align(Alignment.Start))
+                //El OutlinedTextField para el username
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Introduce tu email", color = negro.copy(alpha = 0.7f)) },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = rojo,
+                        focusedBorderColor = rojo,
+                        cursorColor = rojo,
+                        unfocusedLabelColor = negro,
+                        focusedLabelColor = negro
+                    )
+                )
+
+
+                //El OutlinedTextField para la contrasenia
+                Text("Contraseña",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = negro, modifier = Modifier
+                        .align(Alignment.Start))
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Introduce tu contraseña", color = negro.copy(alpha = 0.7f)) },
+                    singleLine = true,
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        val icon = if (passwordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(imageVector = icon, contentDescription = null)
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = OutlinedTextFieldDefaults.colors(
+                        unfocusedBorderColor = rojo,
+                        focusedBorderColor = rojo,
+                        cursorColor = rojo,
+                        unfocusedLabelColor = negro,
+                        focusedLabelColor = negro
+                    )
+                )
+
+                // Botón Iniciar Sesion
+                Button(
+                    onClick = {  },
+                    colors = ButtonDefaults.buttonColors(containerColor = negro),
+                    shape = RoundedCornerShape(14),
+                    modifier = Modifier
+                        .height(56.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        "Crear Cuenta",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        fontSize = 18.sp
+                    )
+                }
+
+                // Botón Limpiar
+                Button(
+                    onClick = {
+                        nombre = ""
+                        email = ""
+                        password = ""
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
+                    shape = RoundedCornerShape(10),
+                    modifier = Modifier
+                        .height(50.dp)
+                        .fillMaxWidth(0.5f)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear",
+                        modifier = Modifier.size(20.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text("Limpiar")
+                }
+
+                // Frase registro
+                Row(
+                    modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "¿Ya tienes cuenta? ", color = negro)
+                    Text(
+                        text = "Iniciar sesion",
+                        fontWeight = FontWeight.Bold,
+                        color = negro
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RegistroPreview() {
+    Registro()
+}
